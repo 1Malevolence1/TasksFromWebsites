3337e940d9213a69815955d541d8ee6758d7b3ba
Mr.D <danil36rusdanil@mail.ru>
2024-07-23 12:28:57 +0300

    refactore code. Remove костыль

--- src/main/java/org/example/LeetCode/MaximumAreaOfLongestDiagonalRectangle3000.java
+++ src/main/java/org/example/LeetCode/MaximumAreaOfLongestDiagonalRectangle3000.java
@@ -11,27 +11,24 @@ public class MaximumAreaOfLongestDiagonalRectangle3000 {
 
 
 
-        System.out.println(areaOfMaxDiagonal(new int[][]{{1,10}, {3,10}, {4,4}, {2,6}, {6,3}, {6,4}, {9,1
-        }, {6,1}, {2,3}}));
+        System.out.println(areaOfMaxDiagonal(new int[][]{{2,6},{5,1},{3,10},{8,4}}));
     }
 
-    public  static int areaOfMaxDiagonal(int[][] dimensions){
+    public  static int areaOfMaxDiagonal(int[][] dimensions) {
         int area = 1;
         double maxDiagonal = Integer.MIN_VALUE;
 
         for (int i = 0; i < dimensions.length; i++) {
             double diagonal = 0;
             for (int j = 0; j < dimensions[i].length;  j++) {
-            diagonal  += Math.pow(dimensions[i][j], 2);
-                if(diagonal >= maxDiagonal){
-                    maxDiagonal = diagonal;
-                    area = dimensions[i][0] * dimensions[i][1];
-                }
+                diagonal  += Math.pow(dimensions[i][j], 2);
+            }
+            if(diagonal > maxDiagonal || (diagonal == maxDiagonal && dimensions[i][0] * dimensions[i][1] > area)){
+                maxDiagonal = diagonal;
+                area = dimensions[i][0] * dimensions[i][1];
             }
         }
-
-
-        if(area == 1848) return 2028;
         return area;
     }
 }
+
