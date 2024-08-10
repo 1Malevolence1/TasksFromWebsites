6a4b3fc1a8e4db4260697b5ede44bd5df822df3f
Mr.D <danil36rusdanil@mail.ru>
2024-08-10 14:17:28 +0300

    solve task

---
+++ src/main/java/org/example/LeetCode/ToLowerCase709.java
@@ -0,0 +1,13 @@
+package org.example.LeetCode;
+
+public class ToLowerCase709 {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
