9482d9ee3e4dba936d8e602f14d4825314161e02
Mr.D <danil36rusdanil@mail.ru>
2024-09-10 18:47:18 +0300

    solve task

---
+++ src/main/java/org/example/LeetCode/MajorityElement169.java
@@ -0,0 +1,12 @@
+package org.example.LeetCode;
+
+import java.util.Arrays;
+
+public class MajorityElement169 {
+
+
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
+    }
+}
